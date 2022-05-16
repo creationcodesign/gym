fb7fec609c6d1265d394ec5a6b751efbad4c712e
Natalia <adelina09715@gmail.com>
2022-05-16 15:12:02 +0200

    responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import '../style/navbar.scss'
 
 export default function Navbar() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(false)
+
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton)
+
     return (
         <nav>
-            <NavLink to="/" className="navbar-logo">
+            <NavLink to="/" className="navbar-logo" onClick={closeMobileMenu}>
                 <i className="fas fa-dumbbell logo-icon"></i>
                 <p>GYM</p>
             </NavLink>
-            <NavLink to="/services" className="nav-link" activeclassname="active-link">Services</NavLink>
-            <NavLink to="/membership" className="nav-link" activeclassname="active-link">Membership</NavLink>
-            <NavLink to="/contact" className="nav-link" activeclassname="active-link">Contact</NavLink>
-            <NavLink to="/authorize" className="nav-link" activeclassname="active-link">Sign Up</NavLink>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? "fas fa-times icon" : "fas fa-bars icon"} />
+            </div>
+            <div className={click ? "nav-menu active" : "nav-menu"}>
+                <NavLink to="/services" className="nav-link" activeclassname="active-link" onClick={closeMobileMenu}>Services</NavLink>
+                <NavLink to="/membership" className="nav-link" activeclassname="active-link" onClick={closeMobileMenu}>Membership</NavLink>
+                <NavLink to="/contact" className="nav-link" activeclassname="active-link" onClick={closeMobileMenu}>Contact</NavLink>
+            </div>
+            {button && <button className='btn-outline btn-small'>SIGN UP</button>}
         </nav>
     )
 }
