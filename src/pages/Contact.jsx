14440b30dd7a84a40d30150d14494a1d03206ae7
Natalia <adelina09715@gmail.com>
2022-05-18 09:37:50 +0200

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Contact() {
     return (
-        <div>Contact</div>
+        <div className="contact-container">
+            <h2>Contact</h2>
+            <p>please do not hesitate to send us a message</p>
+            <form>
+                <input type="text" className="form-control" placeholder="NAME" name="name" value="" />
+                <input type="email" className="form-control" placeholder="EMAIL" name="email" value="" />
+                <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message"></textarea>
+                <button className="btn send-button" type="submit" value="SEND">
+                    <div className="btn-icon">
+                        <i className="fa fa-paper-plane"></i><span className="send-text">SEND</span>
+                    </div>
+                </button>
+            </form>
+        </div>
     )
 }
